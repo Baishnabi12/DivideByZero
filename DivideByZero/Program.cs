07a4b528fa22e57882b6bd8f84d36e8b8d84596a
baish <baish@BAISHNABI>
2024-10-17 13:29:27 +0530

    Add project files.

---
+++ DivideByZero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class DivideZero
+{
+    static void Main(string[] args)
+    {
+        int number1 = 10;
+        int number2 = 0;
+        try
+        {
+            Console.WriteLine(number1/number2); 
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+}
